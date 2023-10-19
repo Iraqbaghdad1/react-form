ebb76474be2c086c534bc30d17cfa65636e1946c
Iraqbaghdad1 <134105675+Iraqbaghdad1@users.noreply.github.com>
2023-10-19 06:55:26 +0500

    updated app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductForm from './ProductForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductForm />
+    </div>
+  );
+}
+
+export default App;
+
